f4b2c6a6a2c0acff00a9ba52972f52b204d423df
faxriddin <faxriddin@gmail.com>
2024-03-27 10:55:31 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+
+let userNum = +prompt("istalgan soni kiriti");
+
+for (let i = 0; i <= userNum; i++) {
+  let name = prompt("User name", "faxriddin");
+  let age = +prompt("User age", "13");
+  console.log(`${i+1}  
+   ismi ${name}:
+ yoshi ${age}  `);
+}
